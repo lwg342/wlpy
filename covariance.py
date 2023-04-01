8fb70aec012e21ce3d44ebe053d15f0409e81d05
lwg342@gmail.com <lwg342@gmail.com>
2023-04-01 14:06:07 +0100

    Add a method to do soft thresholding on the correlation matrix

--- covariance.py
+++ covariance.py
@@ -4,44 +4,66 @@
 from sklearn import covariance as sk_cov
 import nonlinshrink as nls
 import numpy as np
+from wlpy.gist import generalized_threshold
 # %%
 
 
 class Covariance():
-    def __init__(self, DF_TxN):
+    """
+    Several Estimators for Covariance Matrix Given a Sample of Size T*N or N*p
+    """
+
+    def __init__(self, DF_TxN=np.eye(2)):
         """
         Covariance estimation from the observations X:T*N
         """
         self.X = DF_TxN
         self.T, self.N = DF_TxN.shape
-        self.S_sample = self.sample_cov()
-        self.S_lw = self.lw_lin_shrink()
-        # self.S_nlshrink = self.nonlin_shrink()
+        self.sample_cov_estimate = self.sample_cov()
 
     def sample_cov(self):
-        _S = np.cov(np.array(self.X), rowvar=False)
-        return _S
+        sample_cov_estimate = np.cov(np.array(self.X), rowvar=False)
+        return sample_cov_estimate
 
-    def lw_lin_shrink(self):
+    def linear_shrinkage(self):
         """
-        lw stands for Ledoit and Wolf 2004
+        Ledoit and Wolf 2004
         """
         S_lw = sk_cov.LedoitWolf().fit(self.X).covariance_
         return S_lw
 
-    def nonlin_shrink(self):
+    def nonlinear_shrinkage(self):
         S_nlshrink = nls.shrink_cov(self.X)
         return S_nlshrink
 
-    def network_hard_threshold(self, G, est_cov=None):
+    def network_hard_threshold(self, G, cov_mat=None):
         """
         This calculates the hard-threshold estimator using a network matrix G. 
-        The original estimator is est_cov
+        The original estimator is cov_mat 
         """
-        if est_cov == None:
-            _S = self.sample_cov()
+        if cov_mat == None:
+            _S = self.sample_cov_estimate
         else:
-            _S = est_cov
+            _S = cov_mat
         _S[np.where((G + np.eye(self.N)) == 0)] = 0
         return _S
-# %%  Heatmap of the cov matrix S
+
+    def threshold_corr(self, cov_mat=None, thresholding_method="soft", regularization_constant=2):
+        if cov_mat == None:
+            cov_mat = self.sample_cov_estimate
+        else:
+            cov_mat = cov_mat
+        rate = np.sqrt((np.log(self.N))/self.T)
+
+        if thresholding_method == "soft":
+            threshold_matrix = np.ones(
+                (self.N, self.N))*rate*regularization_constant
+            threshold_matrix = threshold_matrix - \
+                np.diag(np.diag(threshold_matrix))
+
+            diag_vals = np.sqrt(np.diag(cov_mat))
+            corr_mat = cov_mat / np.outer(diag_vals, diag_vals)
+
+            return np.outer(diag_vals, diag_vals) * generalized_threshold(corr_mat, threshold_matrix, method="soft threshold")
+        else:
+            raise NotImplementedError("Other methods are not implemented yet")
